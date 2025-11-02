f4c87e8b4e9f187c09fb03e8107c535df54b5340
mattrock12344-collab <mattrock12344@gmail.com>
2025-11-02 14:18:53 -0500

    5: oh shit I havent been keeping track of changes

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def get_book_text(book_file_path): 
-    # This function reads the content of a book file and returns it as a string.
-    with open(book_file_path) as f: 
-        book_text = f.read()
-    return book_text
+from stats import word_count, char_count
 
-def main():
-    book_file_path = "./books/frankenstein.txt"
-    book_text = get_book_text(book_file_path)
-    words = book_text.split()
-    print(f"Found {len(words)} total words")
-
-main() 
+word_count()
+char_count()
---
+++ stats.py
@@ -0,0 +1,32 @@
+def get_book_text(book_file_path): 
+    # This function reads the content of a book file and returns it as a string.
+    with open(book_file_path) as f: 
+        book_text = f.read()
+    return book_text
+
+def word_count():
+    book_file_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+    words = book_text.split()
+    print(f"Found {len(words)} total words")
+
+def char_count():
+    book_file_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+    lower = book_text.lower()
+    char = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0,
+            'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0, 'n': 0,
+            'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0,
+            'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0, 'æ': 0, 'â': 0,
+            'ê': 0, 'ë': 0, 'ô': 0}
+    for i in lower:
+        if i in char:
+            char[i] += 1
+    return char
+
+def sort():
+    sorted = []
+    char = char_count()
+    for i in char:
+        sorted.append({"char": i, "num": char[i]})
+    
