e562b076d2ac197032730b262f23a9d48c0453db
mattrock12344-collab <mattrock12344@gmail.com>
2025-11-02 13:24:41 -0500

    2: please update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+def get_book_text(book_file_path): 
+    # This function reads the content of a book file and returns it as a string.
+    with open(book_file_path) as f: 
+        book_text = f.read()
+    return book_text
+
+def main():
+    book_file_path = "./books/frankenstein.txt"
+    book_text = get_book_text(book_file_path)
+    print(book_text)
+
+main() 
